2b05841daffd815343de5d75288cba5485766e2c
seanj12c <darkslayer0912@gmail.com>
2023-03-10 19:25:47 +0800

    Change max width and change text sizes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,20 +4,22 @@ import Typed from "react-typed";
 const Hero = () => {
   return (
     <div id="home" className="text-white">
-      <div className="max-w-[800px] w-full h-screen text-center mx-auto flex flex-col justify-center pt-[60px]">
+      <div className="max-w-[1000px] w-full h-screen text-center mx-auto flex flex-col justify-center pt-[60px]">
         <p className="text-primary font-bold p-2">
           Hello <span className="text-white">I am</span>
         </p>
         <h1
           id="buclatin"
-          className="md:text-7xl sm:text-4xl text-3xl font-bold md:py-6"
+          className="md:text-5xl lg:text-7xl sm:text-3xl text-2xl font-bold md:py-6"
         >
           Sean Jimwell Buclatin
         </h1>
         <div id="web" className="flex justify-center items-center">
-          <p className="md:text-5xl sm:text-4xl text-xl font-bold py-4">Web</p>
+          <p className="lg:text-5xl md:text-3xl sm:text-2xl text-xl font-bold py-4">
+            Web
+          </p>
           <Typed
-            className="md:text-5xl sm:text-4xl text-xl font-bold md:pl-4 pl-2 text-primary"
+            className="lg:text-5xl md:text-3xl sm:text-2xl text-xl font-bold md:pl-4 pl-2 text-primary"
             strings={["Developer", "Designer"]}
             typeSpeed={100}
             backSpeed={100}
