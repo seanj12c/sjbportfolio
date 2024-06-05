5a839861fbfdfa1b87d4ad03ef2d000f285d9096
Sean Jimwell Buclatin <105427349+seanj12c@users.noreply.github.com>
2024-06-06 07:26:34 +0800

    remove the home in the home

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { FaFacebook, FaGithub } from "react-icons/fa";
-
+import bg from "../assets/bg.png";
 const Hero = () => {
   return (
     <div id="home" className="text-white">
       <div id="large-header">
+        <img
+          className="z-[-1] absolute opacity-75 h-screen w-screen mx-auto object-contain pointer-events-none select-none blur-sm"
+          src={bg}
+          alt=""
+        />
         <div className="w-full h-[94vh] text-center  flex flex-col justify-center">
           <div className="border px-2 w-72 sm:w-80 md:w-full md:max-w-lg p-10 rounded-lg mx-auto glass">
             <p className="text-accent md:text-2xl font-bold ">
@@ -21,20 +26,25 @@ const Hero = () => {
               </p>
             </div>
             <ul className="flex text-xs md:text-lg justify-center">
-              <Link to="/">
-                <li className="p-3 hover:text-accent">Home</li>
-              </Link>
               <Link to="/about">
-                <li className="p-3 hover:text-accent">About</li>
+                <li className="p-3 hover:text-accent  duration-300 transition-all ease-in-out">
+                  About
+                </li>
               </Link>
               <Link to="/resume">
-                <li className="p-3 hover:text-accent">Resume</li>
+                <li className="p-3 hover:text-accent  duration-300 transition-all ease-in-out">
+                  Resume
+                </li>
               </Link>
               <Link to="/portfolio">
-                <li className="p-3 hover:text-accent">Portfolio</li>
+                <li className="p-3 hover:text-accent  duration-300 transition-all ease-in-out">
+                  Portfolio
+                </li>
               </Link>
               <Link to="/contact">
-                <li className="p-3 hover:text-accent">Contact</li>
+                <li className="p-3 hover:text-accent  duration-300 transition-all ease-in-out">
+                  Contact
+                </li>
               </Link>
             </ul>
             <div className="flex justify-center items-center space-x-4">
@@ -42,6 +52,7 @@ const Hero = () => {
                 href="https://www.facebook.com/seanj12c"
                 target="_blank"
                 rel="noopener noreferrer"
+                className="hover:text-accent duration-300 transition-all ease-in-out"
               >
                 <FaFacebook size={32} />
               </a>
@@ -49,6 +60,7 @@ const Hero = () => {
                 href="https://github.com/seanj12c"
                 target="_blank"
                 rel="noopener noreferrer"
+                className="hover:text-accent duration-300 transition-all ease-in-out"
               >
                 <FaGithub size={32} />
               </a>
